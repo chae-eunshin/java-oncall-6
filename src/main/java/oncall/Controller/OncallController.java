9d3d06927b844e50717a74a7c5709fb589334f8f
shin-chaeeun <chaeeun0217@naver.com>
2024-11-14 20:13:44 +0900

    Feat(OncallController):입력을 핸들링하는 메서드 구현

--- src/main/java/oncall/Controller/OncallController.java
+++ src/main/java/oncall/Controller/OncallController.java
@@ -1,5 +1,6 @@
 package oncall.Controller;
 
+import oncall.utills.ValidationChecker;
 import oncall.view.InputView;
 
 public class OncallController {
@@ -10,6 +11,21 @@ public class OncallController {
     }
 
     public void run(){
-        InputView.readInput("비상 근무를 배정할 월과 시작 요일을 입력하세요>");
+
+        String monthDayInput = InputView.readInput("비상 근무를 배정할 월과 시작 요일을 입력하세요>");
+        ValidationChecker.isDividedByComma(monthDayInput);
+
+    }
+
+    private void hadleMonthDay(){
+        boolean isDone = false;
+        while(!isDone){
+            try {
+                String monthDayInput = InputView.readInput("비상 근무를 배정할 월과 시작 요일을 입력하세요>");
+                isDone= ValidationChecker.isDividedByComma(monthDayInput);
+            }catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
--- src/main/java/oncall/utills/ValidationChecker.java
+++ src/main/java/oncall/utills/ValidationChecker.java
@@ -6,14 +6,20 @@ public class ValidationChecker {
         if(input.matches("[0-9]+,[가-힣]")){
             return true;
         }
-        return false;
+        throw new IllegalArgumentException("[ERROR] 월과 시작 요일은 ,로 구분하여 입력해주세요");
     }
 
     public static boolean isMonth(int month) {
-        return month >= 1 && month <= 12;
+        if(month >= 1 && month <= 12){
+            return true;
+        }
+        throw new IllegalArgumentException("[ERROR] 월은 1월부터 12월 안에서 입력할 수 있습니다.");
     }
 
     public static boolean isDay(String day) {
-        return day.matches("[월화수목금토일]");
+        if(day.matches("[월화수목금토일]")){
+            return true;
+        }
+        throw new IllegalArgumentException("[ERROR] 요일은 월,화,수,목,금,토,일 안에서 입력해주세요");
     }
 }
