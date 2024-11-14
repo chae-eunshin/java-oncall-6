d7db606ce2fd2b52ab96c2b20db3318a2f75345b
shin-chaeeun <chaeeun0217@naver.com>
2024-11-14 21:12:11 +0900

    Feat(OncallController):온콜 스태프 핸들링 메서드 구현

--- src/main/java/oncall/Controller/OncallController.java
+++ src/main/java/oncall/Controller/OncallController.java
@@ -1,6 +1,7 @@
 package oncall.Controller;
 
 import oncall.model.OncallSchedule;
+import oncall.model.OncallStaffManager;
 import oncall.utills.ValidationChecker;
 import oncall.view.InputView;
 
@@ -13,7 +14,7 @@ public class OncallController {
     public void run(){
         String[] monthDayInputs= hadleMonthDay();
         OncallSchedule oncallSchedule = new OncallSchedule(Integer.parseInt(monthDayInputs[0]),monthDayInputs[1]);
-
+        OncallStaffManager oncallStaffManager = new OncallStaffManager(hadleOncallWeekDayStaff()[0],hadleOncallWeekDayStaff()[1] );
     }
 
     private String[] hadleMonthDay(){
@@ -25,6 +26,20 @@ public class OncallController {
                 ValidationChecker.isMonth(Integer.parseInt(inputList[0]));
                 ValidationChecker.isDay(inputList[1]);
                 return inputList;
+            }catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private String[] hadleOncallWeekDayStaff(){
+        while(true){
+            try {
+                String weekdayStaffInput = InputView.readInput("평일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
+                ValidationChecker.isOnCallStaff(weekdayStaffInput);
+                String holidayStaffInput = InputView.readInput("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
+                ValidationChecker.isOnCallStaff(holidayStaffInput);
+                return new String[]{weekdayStaffInput, holidayStaffInput};
             }catch(IllegalArgumentException e){
                 System.out.println(e.getMessage());
             }
